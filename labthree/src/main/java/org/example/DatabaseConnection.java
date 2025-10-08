27e57d4fe690a08fa80d2f02868496752a4820d3
UMURERWA Lisa Ornella <umurerwalisaornella@gmail.com>
2025-10-08 10:54:13 +0200

    Add files via upload
    hided database information on lab three

--- labthree/src/main/java/org/example/DatabaseConnection.java
+++ labthree/src/main/java/org/example/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/school_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "123";
+    private static final String URL = System.getenv("url");
+    private static final String USER =System.getenv("user_name");
+    private static final String PASSWORD = System.getenv("pass");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
